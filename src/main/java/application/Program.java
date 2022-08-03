72dc25030a04e7e45edbcd349aa7d1b9c02d9a9f
Dionatan de Andrade <dionatan.andrade@hotmail.com>
2022-08-03 15:08:48 -0300

    Dependencias MAVEN (hibernate-core, hibernate-entitymanager e mysql-connector-java)

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import entities.Person;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Person p1 = new Person(null, "Carlos da Silva", "carlos@gmail.com");
+        Person p2 = new Person(null, "Joaquim Torres", "joaquim@gmail.com");
+        Person p3 = new Person(null, "Ana Maria", "ana@gmail.com");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+
+        em.getTransaction().commit();
+
+        System.out.println("Done!");
+
+    }
+}
